80eed4bafa13fcd336671f01eeb46fd94f026a09
Webserver User <u328878@klupper.ispgateway.de>
2018-12-10 12:48:35 +0100

    Refactoring, more templates.

--- index.php
+++ index.php
@@ -35,10 +35,12 @@ function download(filename, text) {
     ?></select></td></tr>
     <tr><th>Name:</th><td><input onchange="this.form.submit()" type="text" name="name" value="<?php echo $values[1]; ?>"></td></tr>
     <tr><th>Job title:</th><td><input onchange="this.form.submit()" type="text" name="title" value="<?php echo $values[2]; ?>"></td></tr>
-    <tr><th>Phone:</th><td><input onchange="this.form.submit()" type="text" name="phone" value="<?php echo $values[3] ? $values[3] : "+49 30 2902825 331"; ?>"> (replace extension or whole number!)</td></tr>
-    <tr><th>Additional text:</th><td><input onchange="this.form.submit()" type="text" name="additional" value="<?php echo $values[4]; ?>"> (fax, mobile, …)</td></tr>
+    <tr><th>Phone:</th><td><input onchange="this.form.submit()" type="text" name="phone" value="<?php echo $values[3] ? $values[3] : "+49 30 2902825 331"; ?>"> (replace extension or whole number! Example: +49 30 2902825 331)</td></tr>
+    <tr><th>Additional text:</th><td><input onchange="this.form.submit()" type="text" name="additional" value="<?php echo $values[4]; ?>"> (fax, mobile, GPG fingerprint, …) (Need a line break? Use <code>&lt;br /&gt;</code>)</td></tr>
     <tr><th>Preview:</th><td><input type="submit" value="Preview"><br /><iframe width="700" height="450" name="preview"></iframe><br />Make sure the signature is correct, then:</td></tr>
     <tr><th>Download:</th><td>Click <a href="javascript:download('Signature created by SigTron 3000.html', window.frames[0].document.body.innerHTML);">here</a>, then save to a local file and use it in your email client as HTML signature</td></tr>
   </table>
 </form>
-<p><a href="https://github.com/ProVeg/sigtron">SigTron 3000™ on Github</a></p>
+<p>If you need a special template for you unit, contact Eileen.<br />
+For support with the software, contact Simon.<br />
+<a href="https://github.com/ProVeg/sigtron">SigTron 3000™ on Github</a></p>
